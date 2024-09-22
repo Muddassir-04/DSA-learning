d208d2eb2e1def27f50235d00e1a580ce8c58bc5
Mohd Muddassir Ahmed <muddassirahmed0526@gmail.com>
2024-09-22 23:33:29 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -10,9 +10,23 @@ int sum(int a,int b){
     int sum=a+b;
     return sum;
 }
-int main(){
-    hello();
-    cout<<sum(3,4);
-    return 0;
+
+
+// factorial function
+int factorial(int n){
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact *= i;
+       
+    }
+    return fact;
 }
 
+
+// MAIN FUNCTION
+int main(){
+    hello();   //calling hello() function
+    cout<<sum(3,4)<<endl;   //calling sum() function
+    cout<<factorial(4);   //calling factorial() function
+    return 0;
+}
