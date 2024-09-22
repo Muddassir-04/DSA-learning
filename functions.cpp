f83962cf2085310750e5af9e496613c13ab4c0df
Mohd Muddassir Ahmed <muddassirahmed0526@gmail.com>
2024-09-22 19:18:46 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,11 @@
+#include<iostream>
+using namespace std;
+//function to print hello world
 void hello(){
     cout<<"hello world"<<endl;
 }
+
+//function to calculate sum
 int sum(int a,int b){
     int sum=a+b;
     return sum;
@@ -10,3 +15,4 @@ int main(){
     cout<<sum(3,4);
     return 0;
 }
+
